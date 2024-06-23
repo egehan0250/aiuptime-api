e03bcd3692f440084a02d165b3ba411da39abda9
Egehan <kontasegehan091@gmail.com>
2024-06-23 16:55:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const uptimeDealerAPI = require('aiuptime-api');
 
 (async () => {
-    let uptimeAPI = new uptimeDealerAPI({ key: 'DWDi7MPlRY8Smzbw1ppAF0VrDPGLi25b', api: 'https://aiuptime.net/api/v1' });
+    let uptimeAPI = new uptimeDealerAPI({ key: 'API KEY', api: 'https://aiuptime.net/api/v1' });
 
     let test = await uptimeAPI.test();
-    let createMonitor = await uptimeAPI.createMonitor({ name: 'Test Monitorr', url: 'https://www.google.com', description: 'This is a test monitor' });
+    let createMonitor = await uptimeAPI.createMonitor({ name: 'Test Monitor', url: 'https://www.aiuptime.net', description: 'This is a test monitor' });
 
     //deleteMonitor by monitor URL
-    let deleteMonitor = await uptimeAPI.deleteMonitor({ url: 'https://aiuptime.net1' }); 
+    let deleteMonitor = await uptimeAPI.deleteMonitor({ url: 'https://aiuptime.net' }); 
 
     //deleteMonitor by monitor ID
     let deleteMonitor = await uptimeAPI.deleteMonitor({ monitorID: '3862852262479916500' });
@@ -15,20 +15,21 @@ const uptimeDealerAPI = require('aiuptime-api');
     let getMonitors = await uptimeAPI.getMonitors();
 
     //getMonitor by monitor URL
-    let getMonitor = await uptimeAPI.getMonitor({ url: 'https://brook-stripe-whistle.glitch.me/' });
+    let getMonitor = await uptimeAPI.getMonitor({ url: 'https://www.aiuptime.net' });
 
     //getMonitor by monitor ID
     let getMonitor = await uptimeAPI.getMonitor({ monitorID: '7219057736083927000' });
 
     //monitorUpdate by monitor URL
-    let monitorUpdate = await uptimeAPI.updateMonitor({ name: 'Glitch Test', url: 'https://brook-stripe-whistle.glitch.me/', description: 'This is a test monitor' });
+    let monitorUpdate = await uptimeAPI.updateMonitor({ name: 'Test', url: 'https://www.aiuptime.net', description: 'This is a test monitor' });
 
     //monitorUpdate by monitor ID
-    let monitorUpdate = await uptimeAPI.updateMonitor({ monitorID: '7219057736083927000', name: 'Glitch Test', description: 'This is a test monitor', url: 'https://brook-stripe-whistle.glitch.me/' });
+    let monitorUpdate = await uptimeAPI.updateMonitor({ monitorID: '7219057736083927000', name: 'Test', description: 'This is a test monitor', url: 'https://www.aiuptime.net' });
 
     //changeStatus by monitor URL (statusType: status or maintenance)
-   let changeStatus = await uptimeAPI.changeMonitorStatus({ url: 'https://brook-stripe-whistle.glitch.me',  status: true, statusType: 'status' });
+   let changeStatus = await uptimeAPI.changeMonitorStatus({ url: 'https://www.aiuptime.net',  status: true, statusType: 'status' });
 
     //changeStatus by monitor ID (statusType: status or maintenance)
     let changeStatus = await uptimeAPI.changeMonitorStatus({ monitorID: '7219057736083927000', status: false, statusType: 'maintenance' });
-})();
+
+    })();

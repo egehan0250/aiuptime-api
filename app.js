0934436b6a013d7900726542a92536ab4b2b92eb
Egehan <kontasegehan091@gmail.com>
2024-06-23 17:05:31 +0300

    dev

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+const axios = require("axios");
 
 /*
 
@@ -13,145 +13,155 @@ const axios = require('axios');
 // Please don't remove this code, it's for the uptimeDealerAPI class to work.
 
 class uptimeDealerAPI {
-    constructor(options) {
-        this.key = options.key;
-        this.api = 'https://aiuptime.net/api/v1';
+  constructor(options) {
+    this.key = options.key;
+    this.api = "https://aiuptime.net/api/v1";
+  }
+
+  async test() {
+    try {
+      let res = await axios.post(this.api, {
+        key: this.key,
+        action: "test",
+      });
+      if (res.data.status == "error" || res.data.success == false) {
+        throw new Error(
+          `Error Code: ${res.data.code} Message: ${res.data.message} - ${res.data.errorMesssage}`
+        );
+      }
+      return res.data;
+    } catch (error) {
+      throw new Error(error);
+      return { status: "error", error: error };
     }
+  }
 
-    async test() {
-        try {
-            let res = await axios.post(this.api,
-                {
-                    key: this.key,
-                    action: 'test'
-                });
-                if (res.data.status == 'error' || res.data.success == false) {
-                    throw new Error(`Error Code: ${res.data.code} Message: ${res.data.message} - ${res.data.errorMesssage}`);
-                }
-                return res.data;
-        } catch (error) {
-            throw new Error(error);
-            return { status: 'error', error: error };
-        }
-    };
+  async createMonitor({ name, url, description }) {
+    try {
+      let res = await axios.post(this.api, {
+        key: this.key,
+        action: "createMonitor",
+        name: name,
+        url: url,
+        description: description,
+      });
+      if (res.data.status == "error" || res.data.success == false) {
+        throw new Error(
+          `Error Code: ${res.data.code} Message: ${res.data.message} - ${res.data.errorMesssage}`
+        );
+      }
+      return res.data;
+    } catch (error) {
+      throw new Error(error);
+      return { status: "error", error: error };
+    }
+  }
 
-    async createMonitor({ name, url, description }) {
-        try {
-            let res = await axios.post(this.api,
-                {
-                    key: this.key,
-                    action: 'createMonitor',
-                    name: name,
-                    url: url,
-                    description: description
-                });
-                if (res.data.status == 'error' || res.data.success == false) {
-                    throw new Error(`Error Code: ${res.data.code} Message: ${res.data.message} - ${res.data.errorMesssage}`);
-                }
-                return res.data;
-        } catch (error) {
-            throw new Error(error);
-            return { status: 'error', error: error };
-        }
-    };
+  async deleteMonitor({ monitorID, url }) {
+    // monitorID or url
+    try {
+      let res = await axios.post(this.api, {
+        key: this.key,
+        action: "deleteMonitor",
+        monitorID: monitorID,
+        url: url,
+      });
+      if (res.data.status == "error" || res.data.success == false) {
+        throw new Error(
+          `Error Code: ${res.data.code} Message: ${res.data.message} - ${res.data.errorMesssage}`
+        );
+      }
+      return res.data;
+    } catch (error) {
+      throw new Error(error);
+      return { status: "error", error: error };
+    }
+  }
 
-    async deleteMonitor({ monitorID, url }) { // monitorID or url
-        try {
-            let res = await axios.post(this.api,
-                {
-                    key: this.key,
-                    action: 'deleteMonitor',
-                    monitorID: monitorID,
-                    url: url
-                });
-                if (res.data.status == 'error' || res.data.success == false) {
-                    throw new Error(`Error Code: ${res.data.code} Message: ${res.data.message} - ${res.data.errorMesssage}`);
-                }
-                return res.data;
-        } catch (error) {
-            throw new Error(error);
-            return { status: 'error', error: error };
-        }
-    };
+  async getMonitors() {
+    try {
+      let res = await axios.post(this.api, {
+        key: this.key,
+        action: "getMonitors",
+      });
+      if (res.data.status == "error" || res.data.success == false) {
+        throw new Error(
+          `Error Code: ${res.data.code} Message: ${res.data.message} - ${res.data.errorMesssage}`
+        );
+      }
+      return res.data;
+    } catch (error) {
+      throw new Error(error);
+      return { status: "error", error: error };
+    }
+  }
 
-    async getMonitors() {
-        try {
-            let res = await axios.post(this.api,
-                {
-                    key: this.key,
-                    action: 'getMonitors'
-                });
-                if (res.data.status == 'error' || res.data.success == false) {
-                    throw new Error(`Error Code: ${res.data.code} Message: ${res.data.message} - ${res.data.errorMesssage}`);
-                }
-                return res.data;
-        } catch (error) {
-            throw new Error(error);
-            return { status: 'error', error: error };
-        }
-    };
+  async getMonitor({ monitorID, url }) {
+    // monitorID or url
+    try {
+      let res = await axios.post(this.api, {
+        key: this.key,
+        action: "getMonitor",
+        monitorID: monitorID,
+        url: url,
+      });
+      if (res.data.status == "error" || res.data.success == false) {
+        throw new Error(
+          `Error Code: ${res.data.code} Message: ${res.data.message} - ${res.data.errorMesssage}`
+        );
+      }
+      return res.data;
+    } catch (error) {
+      throw new Error(error);
+      return { status: "error", error: error };
+    }
+  }
 
-    async getMonitor({ monitorID, url }) { // monitorID or url
-        try {
-            let res = await axios.post(this.api,
-                {
-                    key: this.key,
-                    action: 'getMonitor',
-                    monitorID: monitorID,
-                    url: url
-                });
-                if (res.data.status == 'error' || res.data.success == false) {
-                    throw new Error(`Error Code: ${res.data.code} Message: ${res.data.message} - ${res.data.errorMesssage}`);
-                }
-                return res.data;
-        } catch (error) {
-            throw new Error(error);
-            return { status: 'error', error: error };
-        }
-    };
+  async updateMonitor({ monitorID, url, name, description }) {
+    // monitorID or url
+    try {
+      let res = await axios.post(this.api, {
+        key: this.key,
+        action: "monitorUpdate",
+        monitorID: monitorID,
+        url: url,
+        name: name,
+        description: description,
+      });
+      if (res.data.status == "error" || res.data.success == false) {
+        throw new Error(
+          `Error Code: ${res.data.code} Message: ${res.data.message} - ${res.data.errorMesssage}`
+        );
+      }
+      return res.data;
+    } catch (error) {
+      throw new Error(error);
+      return { status: "error", error: error };
+    }
+  }
 
-    async updateMonitor({ monitorID, url, name, description }) { // monitorID or url
-        try {
-            let res = await axios.post(this.api,
-                {
-                    key: this.key,
-                    action: 'monitorUpdate',
-                    monitorID: monitorID,
-                    url: url,
-                    name: name,
-                    description: description
-                });
-                if (res.data.status == 'error' || res.data.success == false) {
-                    throw new Error(`Error Code: ${res.data.code} Message: ${res.data.message} - ${res.data.errorMesssage}`);
-                }
-                return res.data;
-        } catch (error) {
-            throw new Error(error);
-            return { status: 'error', error: error };
-        }
-    };
-
-    async changeMonitorStatus({ monitorID, url, status, statusType }) { // monitorID or url
-        try {
-            let res = await axios.post(this.api,
-                {
-                    key: this.key,
-                    action: 'changeStatus',
-                    monitorID: monitorID,
-                    url: url,
-                    status: status,
-                    statusType: statusType
-                });
-                if (res.data.status == 'error' || res.data.success == false) {
-                    throw new Error(`Error Code: ${res.data.code} Message: ${res.data.message} - ${res.data.errorMesssage}`);
-                }
-                return res.data;
-        } catch (error) {
-            throw new Error(error);
-            return { status: 'error', error: error };
-        }
-    };
-              
+  async changeMonitorStatus({ monitorID, url, status, statusType }) {
+    // monitorID or url
+    try {
+      let res = await axios.post(this.api, {
+        key: this.key,
+        action: "changeStatus",
+        monitorID: monitorID,
+        url: url,
+        status: status,
+        statusType: statusType,
+      });
+      if (res.data.status == "error" || res.data.success == false) {
+        throw new Error(
+          `Error Code: ${res.data.code} Message: ${res.data.message} - ${res.data.errorMesssage}`
+        );
+      }
+      return res.data;
+    } catch (error) {
+      throw new Error(error);
+      return { status: "error", error: error };
+    }
+  }
 }
 
 module.exports = uptimeDealerAPI;
--- index.js
+++ index.js
@@ -3,33 +3,33 @@ const uptimeDealerAPI = require('aiuptime-api');
 (async () => {
     let uptimeAPI = new uptimeDealerAPI({ key: 'API KEY', api: 'https://aiuptime.net/api/v1' });
 
-    let test = await uptimeAPI.test();
-    let createMonitor = await uptimeAPI.createMonitor({ name: 'Test Monitor', url: 'https://www.aiuptime.net', description: 'This is a test monitor' });
+//     let test = await uptimeAPI.test();
+//     let createMonitor = await uptimeAPI.createMonitor({ name: 'Test Monitor', url: 'https://www.aiuptime.net', description: 'This is a test monitor' });
 
-    //deleteMonitor by monitor URL
-    let deleteMonitor = await uptimeAPI.deleteMonitor({ url: 'https://aiuptime.net' }); 
+//     //deleteMonitor by monitor URL
+//     let deleteMonitor = await uptimeAPI.deleteMonitor({ url: 'https://aiuptime.net' }); 
 
-    //deleteMonitor by monitor ID
-    let deleteMonitor = await uptimeAPI.deleteMonitor({ monitorID: '3862852262479916500' });
+//     //deleteMonitor by monitor ID
+//     let deleteMonitor = await uptimeAPI.deleteMonitor({ monitorID: '3862852262479916500' });
 
-    let getMonitors = await uptimeAPI.getMonitors();
+//     let getMonitors = await uptimeAPI.getMonitors();
 
-    //getMonitor by monitor URL
-    let getMonitor = await uptimeAPI.getMonitor({ url: 'https://www.aiuptime.net' });
+//     //getMonitor by monitor URL
+//     let getMonitor = await uptimeAPI.getMonitor({ url: 'https://www.aiuptime.net' });
 
-    //getMonitor by monitor ID
-    let getMonitor = await uptimeAPI.getMonitor({ monitorID: '7219057736083927000' });
+//     //getMonitor by monitor ID
+//     let getMonitor = await uptimeAPI.getMonitor({ monitorID: '7219057736083927000' });
 
-    //monitorUpdate by monitor URL
-    let monitorUpdate = await uptimeAPI.updateMonitor({ name: 'Test', url: 'https://www.aiuptime.net', description: 'This is a test monitor' });
+//     //monitorUpdate by monitor URL
+//     let monitorUpdate = await uptimeAPI.updateMonitor({ name: 'Test', url: 'https://www.aiuptime.net', description: 'This is a test monitor' });
 
-    //monitorUpdate by monitor ID
-    let monitorUpdate = await uptimeAPI.updateMonitor({ monitorID: '7219057736083927000', name: 'Test', description: 'This is a test monitor', url: 'https://www.aiuptime.net' });
+//     //monitorUpdate by monitor ID
+//     let monitorUpdate = await uptimeAPI.updateMonitor({ monitorID: '7219057736083927000', name: 'Test', description: 'This is a test monitor', url: 'https://www.aiuptime.net' });
 
-    //changeStatus by monitor URL (statusType: status or maintenance)
-   let changeStatus = await uptimeAPI.changeMonitorStatus({ url: 'https://www.aiuptime.net',  status: true, statusType: 'status' });
+//     //changeStatus by monitor URL (statusType: status or maintenance)
+//    let changeStatus = await uptimeAPI.changeMonitorStatus({ url: 'https://www.aiuptime.net',  status: true, statusType: 'status' });
 
-    //changeStatus by monitor ID (statusType: status or maintenance)
-    let changeStatus = await uptimeAPI.changeMonitorStatus({ monitorID: '7219057736083927000', status: false, statusType: 'maintenance' });
+//     //changeStatus by monitor ID (statusType: status or maintenance)
+//     let changeStatus = await uptimeAPI.changeMonitorStatus({ monitorID: '7219057736083927000', status: false, statusType: 'maintenance' });
 
-    })();
+})();
